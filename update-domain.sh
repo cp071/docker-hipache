263c47596a0dfd9a5b583dad45df42a9bd2bd08a
Laurent VOULLEMIER <laurent.voullemier@gmail.com>
2016-11-27 11:13:11 +0100

    Use project identifier instead of real site url for changing container ip

--- update-domain.sh
+++ update-domain.sh
@@ -1,22 +1,51 @@
 #!/bin/bash
 
+#################################################
+## Update ip of the website of a given project ##
+#################################################
+
+function redis_cli()
+{
+    # Use tr because redis-cli output non printable characters at the end of the line
+    docker exec -t redis redis-cli --raw $1 | tr -dc "[[:print:]]"
+}
+
 if [[ "$2" == "" ]]
 then
-  echo "Usage ${BASH_SOURCE[0]} domain ip"
-  echo "domain is the domain of the site without http:://" 
+  echo "Usage ${BASH_SOURCE[0]} project ip [domain]"
+  echo "project is the name of the project"
   echo "ip is the new ip of the container"
   exit 1
 fi
 
-name=$(docker exec -t redis redis-cli LRANGE frontend:"$1" 0 0 | cut -d " " -f 2 | cut -d "\"" -f 2)
-
-if [[ "$name" == "" ]]
+if [[ $(redis_cli "EXISTS frontend:$1") == 0 ]]
 then
-  echo "Error: can't find container name for $1"
-  exit 1
+  echo "No registered domain found for project $1"
+  question="Domain (domain of the site without http:://):"
+  echo "$question"
+  read domain
+
+  while [[ "$domain" == "" ]] || [[ "$domain" =~ ^http:// ]]
+  do
+    echo "Empty domain or domain starting by http:// entered"
+    echo "$question"
+    read domain
+  done
+  redis_cli "SET frontend:$1 $domain"
 else
-  docker exec -t redis redis-cli LINSERT frontend:"$1" AFTER "$name" "http://$2" >/dev/null
-  docker exec -t redis redis-cli RPOP frontend:"$1" >/dev/null
+  domain=$(redis_cli "GET frontend:$1")
 fi
+
+
+if [[ $(redis_cli "EXISTS frontend:$domain") == 0 ]]
+then
+  redis_cli "RPUSH frontend:$domain $1"
+  redis_cli "RPUSH frontend:$domain http://$2"
+else
+  name=$(redis_cli LRANGE frontend:"$domain" 0 0 | cut -d " " -f 2 | cut -d "\"" -f 2)
+  redis_cli "LINSERT frontend:$domain AFTER $1 http://$2" >/dev/null
+  redis_cli "RPOP frontend:$domain" >/dev/null
+fi
+
 
 exit 0
